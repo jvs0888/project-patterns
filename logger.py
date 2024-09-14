e9a526f117a018e20f86321a85fd39d21a9c6a56
Yevhen Syzintsev <108722623+jvs0888@users.noreply.github.com>
2024-09-15 00:09:17 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,16 +1,1 @@
-import os
-from loguru import logger
-from datetime import datetime
 
-
-class LoggerMixin:
-    def __init__(self):
-        self.classname = self.__class__.__name__
-        self.date = datetime.now().date()
-        self.logger = logger.bind(classname=self.classname)
-        self.path = os.path.join(os.path.dirname(__file__), 'logs', f'{self.date}.log')
-        self.logger.add(sink=self.path,
-                        backtrace=True,
-                        diagnose=True,
-                        encoding='utf-8',
-                        rotation='5 MB')
